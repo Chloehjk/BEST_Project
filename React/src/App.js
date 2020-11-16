1c5ccbc27691e859a27497b495aaca34803d8313
Kim Hyunjin <1605hjk@gmail.com>
2020-11-16 17:32:22 +0900

    make main page design

--- React/src/App.js
+++ React/src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import 'App.css';
+import 'main.css';
 import'antd/dist/antd.css';
-import MysiteFront from 'MysiteFront';
+import {Menu} from 'antd';
+import img1 from './images/logo1.png';
 import Login from 'account/Login'
 
 function App() {
   return (
-    <div>
-      <MysiteFront/>
-      hello i'm jihyun
+    <div id='total_container'>
+      <div id='top_container'>
+        <img id='logo' src={img1} width='170px'/>
+        <Menu id='main_category' theme="dark" mode="horizontal">
+          <Menu.Item key="1">소개</Menu.Item>
+          <Menu.Item key="2">주식 기초</Menu.Item>
+          <Menu.Item key="3">KOSPI 50</Menu.Item>
+          <Menu.Item key="4">기초 투자방법</Menu.Item>
+        </Menu>
+      </div>
+      <div id='content_gray'>
+        풀 화면으로 회색박스임. 내용2
+      </div>
+      <div id='content_white'>
+        흰색 박스 내용2
+      </div>
     </div>
   );
 }
