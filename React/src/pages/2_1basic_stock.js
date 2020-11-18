b566897c1a654caf1d443dbb3f5b03fcf98f227a
suran <suute2524@gmail.com>
2020-11-18 18:15:53 +0900

    update basic_stock2

--- React/src/pages/2_1basic_stock.js
+++ React/src/pages/2_1basic_stock.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import testimg1 from 'images/StockTerms.png';
-import testimg2 from 'images/Stockbasic.png';
+import blogimage1 from 'images/blog1.PNG';
+import blogimage2 from 'images/blog2.PNG';
+import blogimage3 from 'images/blog3.PNG';
+import blogimage4 from 'images/blog4.PNG';
 import ReactDOM from 'react-dom';
+import 'pages_css/2_1basic_stock.css';
 import 'pages_css/2_2basic_invest.css';
+
 
 
 export default function BasicStock()
@@ -26,52 +30,86 @@ export default function BasicStock()
                 </div>
                 <div id='content_white'>
                     <ul class='title'>1. 주식 기초 개념 익히기</ul>
-                        <li class='idea'> 주식을 처음 시작하려고 하면 많은 주식용어, 복잡한 차트 때문에 어디서부터 접근을 해야될지 어려움이 따르기 마련입니다.  
-                            내용을 입력하세요.<br/>
+                        <li class='idea'> 주식을 처음 시작하려고 하면 많은 주식용어, 복잡한 차트 때문에 어디서부터 접근을 해야될지 어려움이 따르기 마련입니다.<br/>
+                            주린이들도 쉽게 이해할 수 있는 기본지식과 주식용어를 정리해두었으니 차근차근 알아보도록해요!<br/>
                             아래의 블로그 링크를 보며 전반적인 내용을 이해 한 후, 영상을 보시면 더욱 이해하기에 편합니다.</li>
 
-                        <div>
-                            <img id='test1' src={testimg1}/>
-                        </div>
+                            <div id='blog_container'>
+                                <div id='blog_step'>                                    
+                                    <span>step 1👇🏻</span>
+                                    <a href="https://blog.naver.com/e_adventure/221593948809">
+                                        <img id='blog1' src={blogimage1} alt='설명'/>
+                                    </a>
+                                </div>
+                                <div id='blog_step'>                                    
+                                    <span>step 2👇🏻</span>
+                                    <a href="https://www.goodchobo.com/magazine/MZTY/492">
+                                        <img id='blog2' src={blogimage2} alt='설명'/>
+                                    </a>
+                                </div>
+                            </div>
 
-                        <div>
-                            <img id='test1' src={testimg2}/>
-                        </div>
-                        
-                        <li class='idea'>
-                            {/* 블로그 링크 넣기 */}
-                            <br/>
-                            주식에 대한 전반적인 내용이 이해가셨나요? <br/>
-                            그렇다면 이번엔 영상을 통해 더욱 기본기를 다져보는 시간을 가져보시길 바랍니다.</li>
-                            {/* 유튜브 링크넣기 */}
-                            {/* <p align = 'middle'>
-                            <frameset rows="300,*" frameborder="1">
-                            <iframe framespacing="20px" vspace ="20" width="450" height="350" src="https://www.youtube.com/embed/LzXzHoXO5ZE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                            <iframe marginwidth="20px" width="450" height="350" src="https://www.youtube.com/embed/-d291restec" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                            </frameset>
-                            </p> */}
+                        <li class='idea'> 
+                        주식에 대한 전반적인 내용이 이해가셨나요? <br/>
+                        그렇다면 이번엔 영상을 통해 더욱 기본기를 다져보는 시간을 가져보시길 바랍니다.</li>
+                        {/* 유튜브 링크넣기 */}
+                        <p align = 'middle'>
+                        <iframe id='youtube1' src="https://www.youtube.com/embed/LzXzHoXO5ZE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='true'></iframe>
+                        <iframe id='youtube2' src="https://www.youtube.com/embed/-d291restec" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='true'></iframe>
+                        </p>
                     <ul class='title'>2. 주식을 고를때 유의할점!</ul>
                         <li class='idea'> 주식을 고를때에 여러가지 사항을 확인한 후 신중한 선택을 하는 것이 중요한데요, 그 중에서도 '재무제표'를 꼭 참고하여 주식을 고를 필요가 있습니다.<br/> 
                             좋은 주식을 고르기 전 꼭 확인해야 할 기업의 재무제표! 같이 알아볼까요? <br/>
                             내용을 입력하세요.
                             내용을 입력하세요.</li>
+                    <div id = 'youtube_container'>
                         <p align = 'middle'>
-                        <iframe width="300" height="200" src="https://www.youtube.com/embed/92550HuR3Ss" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        <iframe width="300" height="200" src="https://www.youtube.com/embed/UDNbcrIrfzc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        <iframe width="300" height="200" src="https://www.youtube.com/embed/ZpSdwgL5K5A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe id='youtube3' src="https://www.youtube.com/embed/92550HuR3Ss" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='true'></iframe>
+                        <iframe id='youtube4' src="https://www.youtube.com/embed/UDNbcrIrfzc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='true'></iframe>
+                        <iframe id='youtube5' src="https://www.youtube.com/embed/ZpSdwgL5K5A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='true'></iframe>
                         </p>
+                    </div>
+
                     <ul class='title'>3. 주식차트분석</ul>
                         <li class='idea'> 주식차트 분석하는 법 아직 모르시죠? 저희 투현란이 알려드립니다!
                             내용을 입력하세요.<br/>
                             내용을 입력하세요.
                             내용을 입력하세요.</li>
 
+                        {/* 블로그 */}
+                        <div id='blog_container'>
+                                <div id='blog_step'>                                    
+                                    <span>차트용어 알아보기👇🏻</span>
+                                    <a href="https://www.goodchobo.com/magazine/MZTY/533">
+                                        <img id='blog3' src={blogimage3} alt='설명'/>
+                                    </a>
+                                </div>
+                                <div id='blog_step'>                                    
+                                    <span>봉차트 개념 정리하기👇🏻</span>
+                                    <a href="https://blog.naver.com/kiwoomhero/90129539968">
+                                        <img id='blog4' src={blogimage4} alt='설명'/>
+                                    </a>
+                                </div>
+                            </div>
+
+
+                        {/* 유튜브    */}
+                        <p align = 'middle'>
+                        <iframe id='youtube6' src="https://www.youtube.com/embed/HXsuDfgQrtU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='true'></iframe>
+                        <iframe id='youtube7' src="https://www.youtube.com/embed/hI3pzjLbOzY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='true'></iframe>
+                        <iframe id='youtube8' src="https://www.youtube.com/embed/jkgQ3xKilxs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='true'></iframe>
+                        </p>
+
                         <ul class='title'>4. 주식 계좌 개설 방법 안내</ul>
                         <li class='idea'> 주식을 시작하려면 먼저 계좌를 개설해야합니다. 
                             계좌개설이 주식을 시작하기 위한 첫단계라고 말할 수 있죠!<br/>
                             근데 어떻게 해야될지 막막하시죠?
                             저희 투현란이 알려드립니다!</li>
-                    
+                        <p align = 'middle'>
+                        <iframe id='youtube9' src="https://www.youtube.com/embed/h9ikz68Xvv4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='true'></iframe>
+                        <iframe id='youtube10' src="https://www.youtube.com/embed/D7v9thA97tI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='true'></iframe>
+                        </p>
+
                     <div>
                         <a>Fundamental Tool</a>
                     </div>
