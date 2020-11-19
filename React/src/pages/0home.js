dee5be48d28ba6294cf4441e154083bff885da82
Kim Hyunjin <1605hjk@gmail.com>
2020-11-19 12:28:31 +0900

    Upload Banner imgae03

--- React/src/pages/0home.js
+++ React/src/pages/0home.js
@@ -2,13 +2,9 @@ import React,{ Component } from 'react';
 import Slider from "react-slick";
 import 'pages_css/0home.css';
 
-import testimg1 from 'images/test1.jpg';
-import testimg2 from 'images/test2.jpg';
-import testimg3 from 'images/workplace.png';
-import testimg4 from 'images/test3.jpg';
 import main_banner02 from 'images/real_main_banner01.gif';
 import main_banner01 from 'images/real_main_banner02.jpg';
-
+import main_banner03 from 'images/real_main_banner03.jpg';
 
 
 
@@ -34,12 +30,8 @@ export default function Home(){
                     <div>
                         <img id='test1' src={main_banner02}/>
                     </div>
-                    <div id='test2'>
-                        <img  src={testimg1}/>
-                        <h2>사진이랑 글귀 넣어보기~~~</h2>
-                    </div>
                     <div>
-                        <h3>4</h3>
+                        <img id='test1' src={main_banner03}/>
                     </div>
                     </Slider>     
                 </div>
