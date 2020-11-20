903836db8bc331d6938c05c0a2e020d05294a975
suran <suute2524@gmail.com>
2020-11-20 18:18:17 +0900

    변경

--- React/src/pages/1_1int_project.js
+++ React/src/pages/1_1int_project.js
@@ -2,8 +2,6 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import 'pages_css/1_1project.css';
 import project_img from 'images/project.PNG';
-import Texty from 'rc-texty';
-import 'rc-texty/assets/index.css';
 
 
 function IntroProject(){
@@ -21,9 +19,6 @@ function IntroProject(){
                         </ul>  
 
                     </div>
-                    <div className="texty-demo" style={{ marginTop: 64 }}>
-                        <Texty>Ant Motion</Texty>
-                    </div>
                     
                 </div>
                 <img id='project1' src={project_img} alt='이런식으로 할거임'/> 
@@ -31,28 +26,6 @@ function IntroProject(){
             
     )
 }
-
-
-// class Demo extends React.Component {
-//     render() {
-//       return (
-//         <div>
-//           <Parallax
-//             animation={{ x: 0, opacity: 1, playScale: [0.5, 0.8] }}
-//             style={{ transform: 'translateX(-100px)', opacity: 0 }}
-//             className="code-box-shape"
-//           />
-//         </div>
-//       );
-//     }
-//   }
-
-
-// ReactDOM.render(
-//     <Demo/>,
-//     mountNode
-
-// );
 
 export default IntroProject
 
