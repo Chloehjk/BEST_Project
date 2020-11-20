cfc98b61638cfeea14f7f54002f7360fe17068de
jyuny <dlwlgus299@naver.com>
2020-11-20 14:51:16 +0900

    Update Kospi50 content

--- React/src/pages/3kospi50.js
+++ React/src/pages/3kospi50.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'pages_css/3kospi50.css';
 
 
 export default function Kospi50()
@@ -6,8 +7,31 @@ export default function Kospi50()
     return(
         <>
             <div>
-                Kospi50
+                <div id='content_gray3'>
+                    <div class='box'>
+                        <h4 id='title'>✔ KOSPI50 기업별 특징을 알아보세요</h4>
+                        <div id='sub_title'>
+                            <p>① 주식기초백과는 공부하셨나요? 그렇다면 이제는 재무제표를 확인해 볼 때!</p>       
+                            <p>② 2020 급락/급등 point 탐구! 주요 사건 키워드를 워드클라우드를 통해 제공합니다.</p>
+                            <p>③ 관심기업의 최근 동향이 궁금하신가요? 뉴스데이터 분석을 통한 긍정/부정 비율을 확인해보세요.</p>       
+                            <p2 id='comment'>comment by 져니✨</p2>
+                        </div>
+                    </div>
+                </div>
+                
+                <div id='kospi50_list'>
+                    기업리스트 여기
+                </div>
+
+                <div id='content_white3'>
+
+                    <div id='chart'> 
+                        차트는 요기~
+                    </div>
+                  
+                </div>
             </div>
+
 
         </>
         
