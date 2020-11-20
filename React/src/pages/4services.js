6bf786dc29841f4b642b9ca3771eb17592a0586d
Kim Hyunjin <1605hjk@gmail.com>
2020-11-20 12:55:52 +0900

    Put description of services

--- React/src/pages/4services.js
+++ React/src/pages/4services.js
@@ -26,14 +26,36 @@ export default function Services()
     return(
         <>
             <div>
-                <div id='content_gray'>
-                    <div id='select_industry'>
-                        <span>📍업종선택</span>
-                            <select>
-                                {industry.map((v)=>{
-                                    return <option>{v['industry']}</option>
-                                })}
-                            </select>
+                <div id='content_gray4'>
+                    <div class='box'>
+                        <p id='theme4'>✔ BEST에서 제공하는 펀더멘탈 분석</p>
+                        <div>
+                            BEST의 펀더멘탈 분석은 기업의 재무제표에 명시되어 있는 지표들을 활용하여 기업의 기초 체력을 확인하는 작업입니다.<br/>
+                            재무적으로 안정적인지, 미래 성장 가능성이 있는지 등을 볼 수 있습니다.
+                            또한 이를 활용해 기업이 가진 가치가 주가에 반영이 잘 되었는지 아닌지를 판단할 수 있습니다.<br/>
+                            <p id='theme4_1'>BEST 제공 분석 Tool</p>
+                            <p>① 해당 기업의 주식이 저평가됐는지 고평가됐는지 평가하기 위해 <a>PER, PBR</a> 지표를 활용할 것입니다.<br/>
+                            ② 재무 안정도를 평가하기 위해서는 <a>유보율과 부채 비율</a>을 확인해서 평가할 것입니다.</p>
+                            <ul id='comment'>comment by 끌로이 🤔</ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div id='content_white4'>
+                    <div>
+                        <div id='select_industry'>
+                            <span>📍업종선택</span>
+                            <div>
+                                <select>
+                                    {industry.map((v)=>{
+                                        return <option>{v['industry']}</option>
+                                    })}
+                                </select>
+                            </div>
+                        </div>                    
+                        <div id='finance_state'>
+                            djWJfn
+                        </div>
                     </div>
                 </div>
             </div>
