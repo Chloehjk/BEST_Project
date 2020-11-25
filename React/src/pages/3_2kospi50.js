92a229e3d962d01570ff4b18bd8c974f87233a39
jyuny <dlwlgus299@naver.com>
2020-11-25 18:06:18 +0900

    change img names

--- React/src/pages/3_2kospi50.js
+++ React/src/pages/3_2kospi50.js
@@ -3,15 +3,23 @@ import 'pages_css/3kospi50.css';
 import 'pages_css/3_2kospi50.css';
 import pp from 'images/g.png';
 
-export default function Kospi50_2()
+
+
+export default function Kospi50_2({code})
 {
 
+    const [url, setUrl] = React.useState("")
+
+    React.useEffect(()=>{
+        setUrl("https://udgraphimages.s3.ap-northeast-2.amazonaws.com/"+ code +".png")
+    },[code])
 
     return(
         <>
         <div>
-            ghghh
-            <img src={pp}/>
+            
+            <img id="point_chart" src={url}/>
+            
         </div>
         </>
     )
