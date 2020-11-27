99a26e2cfa7725a4c343e66693f786786868829c
Kim Hyunjin <1605hjk@gmail.com>
2020-11-27 18:18:28 +0900

    Make ultimate graph

--- React/src/pages/3_2kospi50.js
+++ React/src/pages/3_2kospi50.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import 'pages_css/3kospi50.css';
 import 'pages_css/3_2kospi50.css';
-import pp from 'images/g.png';
 import Api from '../Api';
-import ReactWordcloud from 'react-wordcloud'
+import ReactWordcloud from 'react-wordcloud';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 
-export default function Kospi50_2({code})
+
+export default function Kospi50_2({code, graphdata, company})
 {
 
     const [url, setUrl] = React.useState("")
@@ -15,17 +17,53 @@ export default function Kospi50_2({code})
 
 
     React.useEffect(()=>{
-        setUrl("https://udgraphimages.s3.ap-northeast-2.amazonaws.com/"+ code +".png")
+        // setUrl("https://udgraphimages.s3.ap-northeast-2.amazonaws.com/"+ code +".png")
 
         Api.get("POINTDATE?code=" + code)
         .then((res)=>{
             const {data} = res;
             setPointdate([data[0]['point1'], data[0]['point2'], data[0]['point3'], data[0]['point4']])
-        })
-
-       
-    },[code])
-
+           // graphdata.closeprice = graphdata.closeprice.map((v)=>{
+           //     return v.closeprice;
+           // })
+            for(let i in graphdata.closeprice) {                
+                if(graphdata.date[i] == data[0]['point1'].replace(/\./gi,"-") || 
+                   graphdata.date[i] == data[0]['point2'].replace(/\./gi,"-") ||
+                   graphdata.date[i] == data[0]['point3'].replace(/\./gi,"-") ||
+                   graphdata.date[i] == data[0]['point4'].replace(/\./gi,"-")) {
+                    graphdata.closeprice[i] = {
+                        y: graphdata.closeprice[i],
+                        marker: {
+                            enabled : true,
+                            symbol: 'circle',
+                            fillColor: '#1B3523',
+                            radius: 6}
+                    }
+                }
+            }
+             setOption({...option,
+            title : {
+                text : '2020 '+company + '의 급락, 급등 포인트'
+            },
+                xAxis : {
+                    categories : graphdata.date,
+                    reversed : true,
+                    type: 'datetime',
+                    labels: {
+                        enabled: false
+                    }
+                },
+                series : [{
+                name : company,
+                marker : {
+                    enabled : false
+                },
+                data : graphdata.closeprice
+            }
+            ]})
+        
+        })},[graphdata])
+            
     const select_wc = (e) =>{
         
         Api.get("MAKEWORDCLOUD?code=" + code + "&date="+ e.target.getAttribute("value"))
@@ -33,33 +71,41 @@ export default function Kospi50_2({code})
             const {data} = res;
             //console.log(data[0]);
             setWc_data(data[0].words.split(','))
-            
-            console.log('jbjbjb')
-            console.log(data[0].words.split(','))
-           
             var count = {};
             data[0].words.split(',').forEach(function(i) { count[i] = (count[i]||0) + 1;});
             var result = Object.keys(count).map((key) => { return {text:key, value:count[key]}});
             setWc_data(result);
         })
-
-        
-        //console.log(count);
-        
-        // const word_cloud = {
-        //     return <ReactWordcloud words={count} />
-        // }
-       
-
     }
+    const [option,setOption] = React.useState({
+            
+            chart: {
+                type: 'spline',
+                width : 600,
+                height : 400
+            },    
+            yAxis : {
+                title : {text : '원'}            
+            },
+            tooltip : {
+                valueSuffix: '원',
+                crosshairs: true,
+                shared: true
+            },
+            colors : ['#76B88A']    
+        })
+        
     
     
 
     return(
         <>
             <div id="point_part">           
-                <img id="point_chart" src={url}/>
-               
+                <HighchartsReact
+                    highcharts={Highcharts}
+                    options={option}/>
+                {/* <img id="point_chart" src={url}/> */}
+
                 <div id='point_dates'>
                     <tr>
                         <p id= 'pointintro'>급락/급등 포인트 날짜</p>
