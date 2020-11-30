604e89f6bc3ca7c717111312c169fd85a20496ff
Kim Hyunjin <1605hjk@gmail.com>
2020-11-30 18:25:12 +0900

    Change for uploading server

--- React/src/Api.js
+++ React/src/Api.js
@@ -2,7 +2,8 @@ import Axios from 'axios';
 
 const api = Axios.create(
 {
-    baseURL: "http://127.0.0.1:8000/Best/",
+    // baseURL: "http://127.0.0.1:8000/Best/",
+    baseURL: "http://54.180.2.181//Best/",
     timeout: 3000,
 });
 export default api;
